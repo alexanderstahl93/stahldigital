d82f2f933e0fa7d87b62587c5d5064b2378cd92c
Alexander <stahl950@gmail.com>
2024-12-04 16:30:34 +0100

    Footer, Logos etc.

---
+++ app/components/cover/index.tsx
@@ -0,0 +1,28 @@
+export default function Example() {
+    return (
+      <div className="bg-white">
+        <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+          <div className="mx-auto max-w-2xl text-center">
+            <h1 className="text-3xl tracking-tight text-gray-900 sm:text-4xl">
+              <strong>Umsatzwachstum</strong> durch SEO, Google Ads & Reputationsmanagement
+            </h1>
+            <p className="mx-auto mt-6 max-w-xl text-pretty text-lg/9 text-gray-600">
+            Wir sorgen für <strong>Umsatzwachstum</strong> bei Dienstleistungsunternehmen in Wien & Umgebung durch SEO, Google Ads & Kundenbewertungen.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-custom-blue px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-custom-orange focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+              >
+                So funktioniert es
+              </a>
+              <a href="#" className="text-sm/6 font-semibold text-gray-900 hover:text-custom-orange">
+                Ihr Investment <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +8,18 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'custom-blue': '#000080',
+        'custom-orange': '#ff6600',
+        'whatsapp': '#25d366',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
